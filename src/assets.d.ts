8cbff331ef69f2d62874a93732dfff10d92795ae
YiJie <51358815+NWYLZW@users.noreply.github.com>
2026-09-19 21:56:04 +0800

    feat: use selected Pet brand icon through public module API (#9)

--- src/assets.d.ts
+++ src/assets.d.ts
@@ -1,3 +1,8 @@
 declare module '*.png' { const url: string; export default url }
 
 declare module '*.svg?url' { const url: string; export default url }
+
+declare module '*.png?inline' {
+  const url: string
+  export default url
+}
